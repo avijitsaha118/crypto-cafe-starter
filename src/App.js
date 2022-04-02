3af71306ca20dcb9ec00098f994a6cd580f9f6d0
avijitsaha118 <sahaavijit908@gmail.com>
2022-04-02 20:19:24 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Homepage from './components/Homepage/Homepage';
+import Header from './components/Header/Header';
+import Coins from './components/Coins/Coins';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import About from './components/About/About';
+import CoinDetails from './components/CoinDetails/CoinDetails';
+import BdAddress from './components/Contact/BdAddress';
+import UsAddress from './components/Contact/UsAddress';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+<div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Homepage/>}></Route>
+        <Route path='/coins' element={<Coins></Coins>}></Route>
+        <Route path='/coin-details/:id' element={<CoinDetails></CoinDetails>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}>
+         
+         <Route path='bd-address' element={<BdAddress></BdAddress>}></Route>
+         <Route path='us-address' element={<UsAddress></UsAddress>}></Route>
+
+        </Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </div>
+
     </div>
   );
 }
---
+++ src/components/CoinDetails/CoinDetails.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Spinner from '../Spinner/Spinner';
+
+const CoinDetails = () => {
+    const {id}= useParams()
+    const [loading, setLoading] =useState(false)
+    const [coin, setCoins] = useState({})
+    useEffect(()=>{
+        setLoading(true)
+        const url =`https://api.coingecko.com/api/v3/coins/${id}`
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>{setCoins(data)
+        setLoading(false)})
+    },[id])
+    return (
+<>
+{loading? <Spinner></Spinner> :
+        <div className='px-4 h-[70vh] pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
+            
+        <div className='h-full grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 content-center'>
+          <div className='order-2 md:order-1'>
+              <h1 className='text-3xl'>
+                  General Info: 
+
+              </h1>
+              <hr/>
+              <h1> Coin Name: {coin.name} </h1>
+              <h1> Market Cap: {coin.market_cap_rank} </h1>
+              <h1> Origin: {coin.country_origin ? coin.country_origin : "Not Available"} </h1>
+
+              <h1 className='text-3xl'>
+                  Scores: 
+
+              </h1>
+              <hr/>
+              <h1>  Community Score: {coin.community_score} </h1>
+              <h1> Developer Score: {coin.developer_score} </h1>
+              <h1> Origin: {coin.country_origin ? coin.country_origin : "Not Available"} </h1>
+          </div>
+          <div className='flex justify-center items-center order-1 md:order-2'>
+              <img src={coin.image?.large} alt="coin-name"/>
+
+          </div>
+
+
+        </div>
+     
+    </div>
+
+}
+</>
+    );
+};
+
+export default CoinDetails;
